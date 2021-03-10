846201d78ce67b81e54353a3b3b55fe9f543d566
MadelineFitzgerald <80349206+MadelineFitzgerald@users.noreply.github.com>
2021-03-09 20:57:31 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Madeline Fitzgerald', // Title for your website.
     tagline: 'Chemistry & CS & Dance!',
-    profilePhoto: 'img/lake.png',
+    profilePhoto: 'img/lake.JPG',
     url: 'https://madelinefitzgerald.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
